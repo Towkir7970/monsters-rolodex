3a3449f6ee61d86b617c4da4c7710d14c48b9aed
Towkir7970 <towkir.aust.110@gmail.com>
2019-11-17 04:18:50 +0600

    Updates

---
+++ precache-manifest.d5ec82c84adf8d14792e105259d54be2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce3c78985b1b8bf62ee45bd5c5a70ab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19d308eba122ef9a6324",
+    "url": "/monsters-rolodex/static/css/main.5bab9d4d.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "19d308eba122ef9a6324",
+    "url": "/monsters-rolodex/static/js/main.7866f0ca.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
